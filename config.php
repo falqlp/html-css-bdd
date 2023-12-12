26d6e542bf85dd6bcb525b605510ab4efd22fced
Ivan-Corneille MAGAGI <ivancocold@gmail.com>
2023-12-12 09:10:53 +0100

    Modification de la chaîne de connexion.

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
-<?php
-$conn = new mysqli("localhost", "user", "test", "REVISIONS");
+y<?php
+$conn = new mysqli("localhost", "user", "test", "humanresources");
 if ($conn->connect_error) {
     die("Échec de la connexion: " . $conn->connect_error);
 }
